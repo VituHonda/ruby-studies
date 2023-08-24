c6d6f7cfb0c83f2c01e594e8149c224ab4742567
vituhonda <vitorutk@gmail.com>
2023-08-24 17:07:37 -0300

    adicionando estudos de input em ruby

---
+++ spd_dio_ruby/entrada.rb
@@ -0,0 +1,14 @@
+# entrada ou input de valores
+# para receber uma entrada utilizasse a seguinte sintaxe:  nomeVariavel = gets
+#nome = gets
+
+#usar o gets adiciona um caractere especial \n no final da string para remove-lo usa: nomeVariavel.chomp
+# nome.chomp
+
+#É possível juntar os dois comandos
+#nome = gets.chomp
+
+#Exemplo
+
+puts "Digite seu nome: "
+nome = gets
