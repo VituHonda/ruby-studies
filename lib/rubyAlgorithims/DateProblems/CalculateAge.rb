b197d57fad775db82fdad31a41ed37bf408b4274
vituhonda <vitorutk@gmail.com>
2023-10-04 15:29:31 -0300

    feat(age): added CalculateAge algorithm

--- lib/rubyAlgorithims/DateProblems/CalculateAge.rb
+++ lib/rubyAlgorithims/DateProblems/CalculateAge.rb
@@ -1,2 +1,27 @@
-# frozen_string_literal: true
+require 'date'
 
+def calcular_idade_detalhada(data_nascimento)
+  hoje = Date.today
+  nascimento = Date.parse(data_nascimento)
+
+  anos = hoje.year - nascimento.year
+  meses = hoje.month - nascimento.month
+  dias = hoje.day - nascimento.day
+
+  if dias < 0
+    meses -= 1
+    dias += 30
+  end
+
+  if meses < 0
+    anos -= 1
+    meses += 12
+  end
+
+  { anos: anos, meses: meses, dias: dias }
+end
+
+data_nascimento = "1990-05-15"
+idade_detalhada = calcular_idade_detalhada(data_nascimento)
+
+puts "Idade: #{idade_detalhada[:anos]} anos, #{idade_detalhada[:meses]} meses, #{idade_detalhada[:dias]} dias."
