56ea013649d607c4ab3e7ed34fa48de5ee92031a
vituhonda <vitorutk@gmail.com>
2023-10-06 09:21:08 -0300

    refator(binarysearch): altered the code to be able to make tests and to correct a logic problem

--- lib/rubyAlgorithims/SearchingAlgorithms/BinarySearch.rb
+++ lib/rubyAlgorithims/SearchingAlgorithms/BinarySearch.rb
@@ -1,24 +1,90 @@
 # binary search - lista numero tem que estar ordenada e lista escrita em ordem
 
-lista = [2,4,6,8,10,12,14,16,18,20]
+class BinarySearch
 
-puts "informe o numero procurado"
-num = gets.to_i
+    def localiza(lista, num)
 
-l = 0 
-r = lista.length - 1
+        if valida(lista)
 
-while l <= r
-    m = l + (r+l)/2
 
-    if lista[m] == num
-        puts "o numero esta na posicao #{m+1}"    
+
+            l = 0
+            r = lista.length - 1
+
+            while l <= r
+                m = (r+l)/2
+
+                if lista[m] == num
+                    return m+1
+                end
+
+                if lista[m] < num
+                    l = m +1
+                else
+                    r = m -1
+                end
+
+            end
+
+            return 'Número não encontrado'
+
+        else
+
+            p lista
+            ordenada = organiza(lista)
+            p ordenada
+
+            l = 0
+            r = ordenada.length - 1
+
+            while l <= r
+                m = (r+l)/2
+
+                if ordenada[m] == num
+                    return m+1
+                end
+
+                if ordenada[m] < num
+                    l = m +1
+                else
+                    r = m -1
+                end
+
+            end
+
+            return 'Número não encontrado'
+
+
+
+        end
+
+
+
     end
 
-    if lista[m] < num 
-        l = m +1
-    else
-        r = m -1
+    def valida(lista)
+
+        i = 0
+        while i < lista.length-1
+
+            if lista[i] > lista[i+1]
+                # p 'false'
+                return false
+            end
+
+            i += 1
+        end
+        # p 'true'
+        return true
+
     end
+
+    def organiza(lista)
+
+        nova = lista.sort
+
+    end
+
 
 end
+
