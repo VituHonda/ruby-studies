ff86ec300b49c4e1bc0dd066b0a32f68b86769d1
vituhonda <vitorutk@gmail.com>
2023-10-06 12:31:04 -0300

    refactor(findfuture): adjust code to be able to make tests #65

--- lib/rubyAlgorithims/DateProblems/FindFuture.rb
+++ lib/rubyAlgorithims/DateProblems/FindFuture.rb
@@ -1,57 +1,53 @@
-def comp(s, t)
-  # Split the date strings when a "/" is found
-  ss = s.split("/")
-  tt = t.split("/")
+class FindFuture
+  def comp(s, t)
+    # Split the date strings when a "/" is found
+    ss = s.split("/")
+    tt = t.split("/")
 
-  date1 = ss.map(&:to_i)
-  date2 = tt.map(&:to_i)
+    date1 = ss.map(&:to_i)
+    date2 = tt.map(&:to_i)
 
-  if date1[2] != date2[2]
-    return date1[2] - date2[2]
-  elsif date1[1] != date2[1]
-    return date1[1] - date2[1]
-  elsif date1[0] != date2[0]
-    return date1[0] - date2[0]
+    if date1[2] != date2[2]
+      return date1[2] - date2[2]
+    elsif date1[1] != date2[1]
+      return date1[1] - date2[1]
+    elsif date1[0] != date2[0]
+      return date1[0] - date2[0]
+    end
+    return 0
   end
-  return 0
-end
 
-def next_closest_date(arr, q)
-  arr.sort! { |a, b| comp(a, b) }
+  def next_closest_date(arr, q)
+    arr.sort! { |a, b| comp(a, b) }
 
-  l = 0
-  r = arr.length - 1
-  ind = -1
+    l = 0
+    r = arr.length - 1
+    ind = -1
 
-  while l <= r
-    m = (l + r) / 2
-    c = comp(q, arr[m])
-    if c == 0
-      ind = m
-      break
-    elsif c < 0
-      r = m - 1
-      ind = m
+    while l <= r
+      m = (l + r) / 2
+      c = comp(q, arr[m])
+      if c == 0
+        ind = m
+        break
+      elsif c < 0
+        r = m - 1
+        ind = m
+      else
+        l = m + 1
+      end
+    end
+
+    if ind == -1
+      return "-1"
     else
-      l = m + 1
+      return arr[ind]
+    end
+  end
+  def perform_queries(arr, q)
+    q.each do |query|
+      return next_closest_date(arr, query)
     end
   end
 
-  if ind == -1
-    return "-1"
-  else
-    return arr[ind]
-  end
 end
-
-def perform_queries(arr, q)
-  q.each do |query|
-    puts next_closest_date(arr, query)
-  end
-end
-
-arr = ["22/4/1233", "1/3/633", "23/5/56645", "4/12/233"]
-
-q = ["23/3/4345", "4/4/34234234", "12/3/2"]
-
-perform_queries(arr, q)
