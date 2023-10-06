e5ed8996d684429e62d2e65b84bf94f86d5fda0e
vituhonda <vitorutk@gmail.com>
2023-10-06 13:36:12 -0300

    refactor(countchar): adjust code to be possible to make tests #55

--- lib/rubyAlgorithims/StringProblems/CountChar.rb
+++ lib/rubyAlgorithims/StringProblems/CountChar.rb
@@ -1,36 +1,49 @@
-def self.count(string)
-  upper = 0
-  lower = 0
-  number = 0
-  special = 0
+class CountChar
 
-  chars = string.chars
+  def count(tipo, string)
+    upper = 0
+    lower = 0
+    number = 0
+    special = 0
 
-  for char in chars
-    if char >= 'A' && char <= 'Z'
-      upper += 1
-    else
-      if char >= 'a' && char <= 'z'
-        lower += 1
+    chars = string.chars
+
+    for char in chars
+      if char >= 'A' && char <= 'Z'
+        upper += 1
       else
-        if char >= '0' && char <= '9'
-          number += 1
+        if char >= 'a' && char <= 'z'
+          lower += 1
         else
-          special += 1
+          if char >= '0' && char <= '9'
+            number += 1
+          else
+            special += 1
+          end
         end
       end
     end
-  end
 
-  p "upper case: #{upper}"
-  p "lower case: #{lower}"
-  p "number case: #{number}"
-  p "special case: #{special}"
+    if tipo == 'upper'
+      return upper
+    end
+
+    if tipo == 'lower'
+      return lower
+    end
+
+    if tipo == 'number'
+      return number
+    end
+
+    if tipo == 'special'
+      return special
+    end
+
+
+  end
 
 end
 
-string1 = "#GeeKs01fOr@gEEks07"
-string2 = "*GeEkS4GeEkS*"
 
-count(string1)
-count(string2)
+
