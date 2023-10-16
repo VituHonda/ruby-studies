7b4ffe3f86ddf60bc8c1dd613cfa8cc4679d981b
vituhonda <vitorutk@gmail.com>
2023-10-16 15:14:29 -0300

    refactor(jobsequencing): removed blank lines on code

--- lib/rubyAlgorithmsTwo/greedyAlgorithms/JobSequencing.rb
+++ lib/rubyAlgorithmsTwo/greedyAlgorithms/JobSequencing.rb
@@ -1,38 +1,37 @@
 class JobSequencing
 
   attr_accessor :id, :deadline, :profit
-    def initialize(id = '', deadline = 0, profit = 0)
-      @id = id
-      @deadline = deadline
-      @profit = profit
-    end
+  def initialize(id = '', deadline = 0, profit = 0)
+    @id = id
+    @deadline = deadline
+    @profit = profit
+  end
 
-    def print(arr, t)
-      n = arr.length
+  def print(arr, t)
+    n = arr.length
 
-      arr.sort! { |a, b| b.profit <=> a.profit }
+    arr.sort! { |a, b| b.profit <=> a.profit }
 
-      result = Array.new(t, false)
+    result = Array.new(t, false)
 
-      job = Array.new(t, nil)
+    job = Array.new(t, nil)
 
-      for i in 0...n
-        j = [t - 1, arr[i].deadline - 1].min
+    for i in 0...n
+      j = [t - 1, arr[i].deadline - 1].min
 
-        while j >= 0
-          # Free slot found
-          if result[j] == false
-            result[j] = true
-            job[j] = arr[i].id
-            break
-          end
-          j -= 1
+      while j >= 0
+        # Free slot found
+        if result[j] == false
+          result[j] = true
+          job[j] = arr[i].id
+          break
         end
+        j -= 1
       end
-
-
-      return job
     end
 
+
+    return job
+  end
 
 end
