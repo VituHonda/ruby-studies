79da8c95c9a942fc0d912910cf009eb8deae1e63
vituhonda <vitorutk@gmail.com>
2023-10-19 13:17:52 -0300

    feat(RunningSumOf1DayArray): made the file of the class

---
+++ lib/LeetCode/RunningSumOf1DArray.rb
@@ -0,0 +1,12 @@
+# 1480
+#
+#   Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]). Return the running
+#   sum of nums.
+#
+#   Dado um array de numeros. Percorra o array e returne a soma de todos os elementos até aquele index.
+
+class RunningSumOf1DayArray
+
+
+
+end
